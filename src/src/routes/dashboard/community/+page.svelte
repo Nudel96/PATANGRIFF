2c0a2107ad7f960e84bc9321faf994eb661492d5
Nudel96 <129609642+Nudel96@users.noreply.github.com>
2025-08-24 17:44:58 +0200

    Remove leftover Svelte files and clean build cache

--- src/src/routes/dashboard/community/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	import MemberCommunity from '$lib/components/MemberCommunity.svelte';
-</script>
-
-<MemberCommunity />
--- src/src/routes/dashboard/heatmap/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	import CurrencyHeatmap from '$lib/components/CurrencyHeatmap.svelte';
-</script>
-
-<CurrencyHeatmap />
